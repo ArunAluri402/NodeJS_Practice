ff76f551ff3eba0317f57a31682bf5037c69e3ea
ArunAluri1576 <aluri.arun.prakash@gmail.com>
2022-10-10 22:46:17 +0530

    first commit

---
+++ assignmentOperators.js
@@ -0,0 +1,37 @@
+//Assignment Operators
+
+// = (assign)
+
+let x = 15;
+let y = 10;
+console.log("x = ", x, "y = ", y);
+
+// (+=)
+
+let z = (x += y);
+console.log("x += y", z);
+
+// (-=)
+
+let a = (x -= y);
+console.log("x -= y", a);
+
+// (*=)
+
+let b = (x *= y);
+console.log("x *= y", b);
+
+// (/=)
+
+let c = (x /= y);
+console.log("x /= y", c);
+
+// (%=)
+
+let d = (x %= y);
+console.log("x %= y", d);
+
+// (**=)
+
+let e = (x **= y);
+console.log("x**=y", e);
