da851f8ebd56bb2b589c4622cc8a7a0119e41834
ArunAluri1576 <aluri.arun.prakash@gmail.com>
2022-10-11 21:38:25 +0530

    second commit

--- dataTypes.js
+++ dataTypes.js
@@ -71,5 +71,88 @@ console.log(typeof concat8); //string
 //number operations
 
 let sum = num +num2; //number + number
-console.log(sum);
-console.log(typeof(sum));
+console.log(sum); 
+console.log(typeof(sum));//number
+
+let sum2 = num2 + c; // number + undefined
+console.log(sum2); //NaN
+console.log(typeof(sum2)); //number
+
+let sum3 = num2 + val; //number + null
+console.log(sum3);
+console.log(typeof(sum3)); //number
+
+let sum4 = num2 + x; //number + boolean
+console.log(sum4);
+console.log(typeof(sum4)); //number (true =1 , false =0)
+
+//undefined
+
+let value = c + d; //undefined + undefined
+console.log(value); // NaN
+console.log(typeof(value)); //number
+
+let value2 = c+str; // undefined + string
+console.log(value2);
+console.log(typeof(value2)); //string
+
+let value3 = c + num; //undefined + number
+console.log(value3); //NaN
+console.log(typeof(value3)); //number
+
+let value4 = d+val; //undefined + null
+console.log(value4); //NaN
+console.log(typeof(value4)); //number
+
+let value5 = d + x; //undefined + boolean
+console.log(value5);
+console.log(typeof(value5)); //number
+
+
+// null operations
+
+let res = val + num; // null + number
+console.log(res);
+console.log(typeof(res)); //number
+
+let res2 = val + val; // null + null
+console.log(res2); // 0
+console.log(typeof(res2)); //number
+
+let res3 = val + c; //null + undefined
+console.log(res3);
+console.log(typeof(res3)); //number
+
+let res4 = val + x; // null + boolean
+console.log(res4);
+console.log(typeof(res4)); //number
+
+//boolean operations
+
+let op = x + str; // boolean + string
+console.log(op);
+console.log(typeof(op)); //string
+
+let op2 = x + y;
+console.log(op2);
+console.log(typeof(op2)); //number
+
+let op3 = x - y;
+console.log(op3);
+console.log(typeof(op3)); //number
+
+let op4 = x + x;
+console.log(op4);
+console.log(typeof(op4)); //number
+
+let op5 = y+y;
+console.log(op5);
+console.log(typeof(op5)); //number
+
+let op6 = x + c; // boolean + undefined
+console.log(op6);
+console.log(typeof(op6)); //number
+
+let op7 = x + val // boolean + null
+console.log(op7);
+console.log(typeof(op7)); //number
